0add7ec14f9a3e2e2a5d86fa795f1ba5091acb8f
Mahil27 <149046006+Mahil27@users.noreply.github.com>
2026-01-28 01:57:22 +0530

    Reduce embedding model size for Render Free

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,5 +5,4 @@ load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 LLM_MODEL = "llama-3.1-8b-instant"
-
-EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+EMBEDDING_MODEL = "sentence-transformers/paraphrase-MiniLM-L3-v2"
